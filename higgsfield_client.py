63c8b3865061b4e2c220042e37fb31978a55d935
lalithaptori07 <aptorigtm@aptori.com>
2026-06-24 04:13:34 +0530

    auto: sync changes

--- higgsfield_client.py
+++ higgsfield_client.py
@@ -469,6 +469,7 @@ class HiggsFieldClient:
             result_url = result_url or data.get("url", "")
         except Exception:
             pass
+        result_url = result_url.rstrip('.,)"\'\\')
 
         self._log.append({
             "type": "video", "model": model, "topic": topic,
